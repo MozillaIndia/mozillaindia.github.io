3b5cd3635a47ccf70c1b25d779ab37cea8fb13ab
Harshil Agrawal <ghagrawal17@gmail.com>
2019-07-21 19:39:56 +0530

    made header responsive. Fixes #153

--- src/components/header.js
+++ src/components/header.js
@@ -1,38 +1,81 @@
-import React from "react"
+import React from 'react'
+import { Link } from 'gatsby'
 import logo from '../images/mozilla-wordmark.svg'
 
+class Header extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      active: false,
+      navBarActiveClass: '',
+    }
+  }
 
-const Header = () => (
-  <header>
-    <nav className="navbar" role="navigation" aria-label="main navigation">
-            <div className="navbar-brand">
-                <a className="navbar-item" href="https://mozillaindia.org">
-                    <img src={logo} width="112" height="32" alt="mozilla logo" /><span id="mozilla-india-wordmark"> india &nbsp;</span>
-                </a>
-                <a role="button" className="navbar-burger burger" aria-label="menu" aria-expanded="false" data-target="navbarBasicExample">
-                    <span aria-hidden="true"></span>
-                    <span aria-hidden="true"></span>
-                    <span aria-hidden="true"></span>
-                </a>
+  toggleHamburger = () => {
+    // toggle the active boolean in the state
+    this.setState(
+      {
+        active: !this.state.active,
+      },
+      // after state has been updated,
+      () => {
+        // set the class in state for the navbar accordingly
+        this.state.active
+          ? this.setState({
+              navBarActiveClass: 'is-active',
+            })
+          : this.setState({
+              navBarActiveClass: '',
+            })
+      }
+    )
+  }
+
+  render() {
+    return (
+      <header>
+        <nav className="navbar" role="navigation" aria-label="main navigation">
+          <div className="navbar-brand">
+            <Link to="/">
+              <img src={logo} width="112" height="32" alt="mozilla logo" />
+              <span id="mozilla-india-wordmark"> india &nbsp;</span>
+            </Link>
+            <div
+              className={`navbar-burger burger ${this.state.navBarActiveClass}`}
+              data-target="navMenu"
+              onClick={() => this.toggleHamburger()}
+            >
+              <span />
+              <span />
+              <span />
             </div>
-            <div id="navbarBasicExample" className="navbar-menu">
-                <div className="navbar-start">
-                    <a className="navbar-item" href="https://blog.mozillaindia.org">
-                        Blog
-                    </a>
-                    <a className="navbar-item" href="https://foundation.mozilla.org">
-                        Foundation
-                    </a>
-                    <a className="navbar-item" href="https://mozilla.org">
-                        Global
-                    </a>
-                    <a className="navbar-item" href="https://activate.mozilla.community/">
-                        Activate
-                    </a>
-                </div>
+          </div>
+          <div
+            id="navMenu"
+            className={`navbar-menu ${this.state.navBarActiveClass}`}
+          >
+            <div className="navbar-start">
+              <a className="navbar-item" href="https://blog.mozillaindia.org">
+                Blog
+              </a>
+              <a className="navbar-item" href="https://foundation.mozilla.org">
+                Foundation
+              </a>
+              <a className="navbar-item" href="https://mozilla.org">
+                Global
+              </a>
+              <a
+                className="navbar-item"
+                href="https://activate.mozilla.community/"
+              >
+                Activate
+              </a>
             </div>
+          </div>
         </nav>
-  </header>
-)
+      </header>
+    )
+  }
+}
 
 export default Header
