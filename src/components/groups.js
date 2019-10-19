f5a5ee9d439954b1c22c7885fe38c4555a6e8515
Mutahar-Majid <Mutahar-Majid@github.com>
2019-10-19 20:09:40 +0530

    Replaced text links for regional groups with icons. Fix #162

--- src/components/groups.js
+++ src/components/groups.js
@@ -1,34 +1,71 @@
 import React from 'react'
+import {
+  FaTwitterSquare,
+  FaFacebookSquare,
+  FaInstagram,
+  FaTelegramPlane,
+  FaGitSquare,
+  FaGlobe,
+  FaMeetup,
+} from 'react-icons/fa'
 
-const Groups = ({ name, data, description }) =>(
-    <div className="tile is-ancestor is-vertical">
-                    <div className="tile is-parent">
-                        <div className="tile is-child box">
-                            <div className="tile is-ancestor is-vertical">
-                                <div className="tile is-parent is-vertical">
-                                    <p className="title">{name}</p>
-                                    {description}
-                                </div>
-        <div className="columns is-multiline">
-    {data.edges.map(({node})=>(
-        <div className="column is-4" key={node.id}>
-            <div className="tile is-child box">
-                <p className="subtitle">{node.name}</p>
-                {node.website?<a href={node.website}>Website </a> :null}
-                {node.telegram?<a href={node.telegram}>Telegram </a> :null}
-                {node.twitter?<a href={node.twitter}>Twitter </a> :null}
-                {node.github?<a href={node.github}>GitHub </a> :null}
-                {node.instagram?<a href={node.instagram}>Instagram </a> :null}
-                {node.facebook?<a href={node.facebook}>Facebook </a> :null}
-                {node.meetup?<a href={node.meetup}>Meetup </a> :null}
-            </div>
+const Groups = ({ name, data, description }) => (
+  <div className="tile is-ancestor is-vertical">
+    <div className="tile is-parent">
+      <div className="tile is-child box">
+        <div className="tile is-ancestor is-vertical">
+          <div className="tile is-parent is-vertical">
+            <p className="title">{name}</p>
+            {description}
+          </div>
+          <div className="columns is-multiline">
+            {data.edges.map(({ node }) => (
+              <div className="column is-4" key={node.id}>
+                <div className="tile is-child box">
+                  <p className="subtitle">{node.name}</p>
+                  {node.website ? (
+                    <a href={node.website}>
+                      <FaGlobe size="24px" color="#1da1f2" />
+                    </a>
+                  ) : null}
+                  {node.telegram ? (
+                    <a href={node.telegram}>
+                      <FaTelegramPlane size="24px" color="#1da1f2" />{' '}
+                    </a>
+                  ) : null}
+                  {node.twitter ? (
+                    <a href={node.twitter}>
+                      <FaTwitterSquare size="24px" color="#1da1f2" />{' '}
+                    </a>
+                  ) : null}
+                  {node.github ? (
+                    <a href={node.github}>
+                      <FaGitSquare size="24px" color="#ad5c51" />
+                    </a>
+                  ) : null}
+                  {node.instagram ? (
+                    <a href={node.instagram}>
+                      <FaInstagram size="24px" color="#3f729b" />{' '}
+                    </a>
+                  ) : null}
+                  {node.facebook ? (
+                    <a href={node.facebook}>
+                      <FaFacebookSquare size="24px" color="#1da1f2" />
+                    </a>
+                  ) : null}
+                  {node.meetup ? (
+                    <a href={node.meetup}>
+                      <FaMeetup size="24px" color="#1da1f2" />
+                    </a>
+                  ) : null}
+                </div>
+              </div>
+            ))}
+          </div>
         </div>
-    ))}
+      </div>
     </div>
-    </div>
-    </div>
-    </div>
-    </div>
+  </div>
 )
 
 export default Groups
